d64b4bc8cc9a528b2d7c2c9651cae9ce4ba9e3f1
“pratik” <pratikbhandurge2222@gmail.com>
2022-08-18 12:25:39 +0530

    Uc3Solution

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace LinkedLists
 {
@@ -9,9 +8,12 @@ namespace LinkedLists
         {
             Console.WriteLine("Welcome to LinkedList Program!");
             LinkedList<string> list = new LinkedList<string>();
-            list.Add("70");
-            list.Add("30");
-            list.Add("56");
+            // list.Add("56");
+            //  list.Add("30");
+            // list.Add("70");
+            list.Append("56");
+            list.Append("30");
+            list.Append("70");
             list.display();
 
         }
